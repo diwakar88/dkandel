b2a0f6c3cfd095e28925355c82032cf48f151f53
diwakar88 <damodarkandel435@gmail.com>
2016-09-25 21:42:33 +0545

    Add files via upload
    WAP to print sum of three no from users.

---
+++ SUM.C
@@ -0,0 +1,25 @@
+/*
+Student Name:Diwakar kandel
+Subject:programming Fund
+Roll No:
+Program:WAP to print sum of three number
+Lab No:01
+Date:25sep,2016
+*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+float a,b,c,sum;
+printf("Enter the sum of three no:");
+printf("Enter the first number:");
+scanf("%f",&a);
+printf("Enter the second no:");
+scanf("%f",&b);
+printf("Enter the second no:");
+scanf("%f",&c);
+sum=a+b+c;
+printf("sum of three no of %f",sum);
+getch();
+}
